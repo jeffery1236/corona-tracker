4278e70ad81f7eac79ec3081faf46ffca1dc46c5
Jeffery <jeffery1236@gmail.com>
2020-06-03 17:17:59 +0800

    Updates

---
+++ precache-manifest.d263cc8b171f4f62e4f6f3045251a13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ad655278f9ccf1166571b0df317639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecdf816701146ede2076",
+    "url": "/static/css/main.d27194e7.chunk.css"
+  },
+  {
+    "revision": "882866e9f2da30d0b532",
+    "url": "/static/js/2.06c2a829.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.06c2a829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecdf816701146ede2076",
+    "url": "/static/js/main.65486c7e.chunk.js"
+  },
+  {
+    "revision": "6741683614b14080d7ee",
+    "url": "/static/js/runtime-main.0805b5a9.js"
+  }
+]);
